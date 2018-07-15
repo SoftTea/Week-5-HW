776c97a7bc0aa27bc27cbf5ec341c589af30fe3a
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-15 14:20:12 -0500

    index.js route setup

--- first-full-stack-homework/controllers/musicController.js
+++ first-full-stack-homework/controllers/musicController.js
@@ -4,6 +4,19 @@ const router = express.Router();
 
 const Music = require('../models/music');
 
+router.get('/', (req,res)=>{
+  
+  Music.find({},(err, allMusic)=>{
+    if(err) {
+      console.log(err, 'error messages on home route')
+    } else {
+      res.render('index.ejs' , {
+        music: allMusic
+      })
+    }
+  })
+})
+
 
 module.exports = router;
 
